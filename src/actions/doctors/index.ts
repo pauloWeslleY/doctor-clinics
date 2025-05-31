b0d65b4d51a874cd019f553c17243b7bb8dfeb93
pauloWeslleY <weslleyspfc13@outlook.com>
2025-05-31 00:06:56 -0300

    feat: add dayjs for time handling in upsertDoctor action with UTC conversion

--- src/actions/doctors/index.ts
+++ src/actions/doctors/index.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import { headers } from "next/headers";
 
 import { db } from "@/db";
@@ -9,9 +11,26 @@ import { actionClient } from "@/lib/safe-actions";
 
 import { UpsertDoctorActionSchema } from "./upsert-doctor.action.schema";
 
+dayjs.extend(utc);
+
 export const upsertDoctor = actionClient
   .schema(UpsertDoctorActionSchema)
   .action(async ({ parsedInput: data }) => {
+    const availableFromTime = data.availableFromTime;
+    const availableToTime = data.availableToTime;
+
+    const availableFromTimeUTC = dayjs()
+      .set("hour", parseInt(availableFromTime.split(":")[0]))
+      .set("minute", parseInt(availableFromTime.split(":")[1]))
+      .set("second", parseInt(availableFromTime.split(":")[2]))
+      .utc();
+
+    const availableToTimeUTC = dayjs()
+      .set("hour", parseInt(availableToTime.split(":")[0]))
+      .set("minute", parseInt(availableToTime.split(":")[1]))
+      .set("second", parseInt(availableToTime.split(":")[2]))
+      .utc();
+
     const session = await auth.api.getSession({
       headers: await headers(),
     });
@@ -27,12 +46,18 @@ export const upsertDoctor = actionClient
     await db
       .insert(doctorsTable)
       .values({
-        id: data.id,
         ...data,
+        id: data.id,
         clinicId: session?.user.clinic?.id,
+        availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+        availableToTime: availableToTimeUTC.format("HH:mm:ss"),
       })
       .onConflictDoUpdate({
         target: [doctorsTable.id],
-        set: { ...data },
+        set: {
+          ...data,
+          availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+          availableToTime: availableToTimeUTC.format("HH:mm:ss"),
+        },
       });
   });
